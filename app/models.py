80bade2cb3f3242cc0f950ece7bba5e026cea24e
dropdead <therealloathing@gmail.com>
2020-04-29 15:38:24 +0300

    added layout

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
+
 class Message(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.TextField()
--- app/views.py
+++ app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Message
+
 
 def main(request):
-    return render(request, 'index.html')
+    messages = Message.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'index.html', {'messages': messages})
